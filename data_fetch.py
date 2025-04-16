887d03d4fc7d24d56e9fae0805069549af2a5595
cukri <sugier.michal26@gmail.com>
2025-04-16 23:36:18 +0200

    downloading historical data

--- data_fetch.py
+++ data_fetch.py
@@ -1,18 +1,37 @@
+from datetime import datetime
 import pandas as pd
 import os
 import requests
 import json
+import time
 
-def load_api_key():
+def load_config(config_path = 'config.json'):
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"No configuration file: {config_path}")
     with open('config.json', 'r') as config_f:
-        return json.load(config_f).get('api_key')
+        config = json.load(config_f)
+    return config
+
+config = load_config()
+BASE_URL = config.get('url')
+COINS_URL = config.get("coins_url")
+
 
 def load_request_params():
-    with open('params.json', 'r') as params_f:
+    with open('top_params.json', 'r') as params_f:
         return json.load(params_f)
 
-def fetch_crypto_data():
-    url = 'https://api.coingecko.com/api/v3/coins/markets'
+def load_crypto_list(path='crypto_list.json'):
+    with open(path, 'r') as crypto_list_f:
+        list = json.load(crypto_list_f)
+    return list['cryptocurrencies']
+
+
+
+
+def fetch_top_crypto_data():
+
+    url = f'{BASE_URL}'
     params = load_request_params()
 
     response = requests.get(url, params=params)
@@ -34,5 +53,42 @@ def fetch_crypto_data():
         print(f'Error request: {response.status_code}')
         print(response.text)
 
-if __name__ == '__main__':
-    fetch_crypto_data()
+
+def fetch_historical_data(start = None, end = None):
+    crypto_ids = load_crypto_list()
+
+    if start is None or end is None:
+        today = datetime.today().strftime('%Y-%m-%d')
+        start = end = today
+    else:
+        if isinstance(start, str):
+                start = datetime.strptime(start, "%Y-%m-%d")
+        if isinstance(end, str):
+                end = datetime.strptime(end, "%Y-%m-%d")
+
+    start_dt = datetime.strptime(start, '%Y-%m-%d')
+    end_dt = datetime.strptime(end, '%Y-%m-%d')
+    start_timestamp = int(start_dt.timestamp())
+    end_timestamp = int(end_dt.timestamp())
+
+
+    for coin_id in crypto_ids:
+        url = f'{COINS_URL}/{coin_id}/market_chart/range'
+        params = {
+            "vs_currency" : "usd",
+            "from": start_timestamp,
+            "to": end_timestamp
+        }
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            prices = data.get('prices', [])
+            df = pd.DataFrame(prices, columns=['timestamp','price'])
+            df.to_csv(f'{coin_id}_historical.csv', index=False)
+            print(f'{coin_id} data saved.')
+        else:
+            print(f'Failed to fetch data for {coin_id}: {response.status_code}')
+
+        time.sleep(1.5)
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pandas as pd
-
-from data_fetch import fetch_crypto_data
+import argparse
+from data_fetch import fetch_top_crypto_data, fetch_historical_data
 from data_engineering import capitalization_categories
 
 
 def main():
-    fetch_crypto_data()
-    df = pd.read_csv('top_crypto.csv')
-    df = capitalization_categories(df)
-    print(df)
+    #fetch_top_crypto_data()
+    #df = pd.read_csv('top_crypto.csv')
+    #df = capitalization_categories(df)
+   # print(df)
+    fetch_historical_data()
 
 if __name__ == '__main__':
     main()
